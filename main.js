7b17d6c2ffde4ed9886c4e92f0d27f1c0da364af
Alapan Chatterjee <chatterjee.alapan07@gmail.com>
2024-11-09 23:36:56 +0530

    refactor: code to use more modern async-await syntax

--- main.js
+++ main.js
@@ -7,24 +7,60 @@ document.addEventListener("DOMContentLoaded", function () {
   const btnConnect = document.getElementById("initiate_connection_btn");
   btnConnect.addEventListener("click", handleEstablishConnection);
 
-  function handleEstablishConnection() {
+  async function handleEstablishConnection() {
     const dc = localConnection.createDataChannel("channel");
-    dc.onmessage = (e) => console.log("Just got a message " + e.data);
-    dc.onopen = (e) => console.log("Connection opened!!");
 
-    localConnection.onicecandidate = () =>
-      sendOfferToRemote(localConnection.localDescription);
+    dc.onmessage = (e) => logger("Client", "Just got a message " + e.data);
+    dc.onopen = (e) => logger("Client", "Connection Opened!!");
 
-    localConnection
-      .createOffer()
-      .then((o) => localConnection.setLocalDescription(o))
-      .then((a) => console.log("Set successfully!"));
+    const offer = await localConnection.createOffer();
+    await localConnection.setLocalDescription(offer);
+    logger("Client", "Session Description Set successfully!");
+    const SDP = await waitToCompleteIceGathering(localConnection);
+    sendOfferToRemote(SDP);
   }
 
-  function sendOfferToRemote(sessionDescription) {
-    console.log(
-      "New Ice Candidate! reprinting SDP" + JSON.stringify(sessionDescription)
-    );
-    const offer = sessionDescription;
+  async function sendOfferToRemote(clientSDP) {
+    const msg = "New Ice Candidate! reprinting SDP" + JSON.stringify(clientSDP);
+    logger("Client", msg);
+
+    const offer = clientSDP;
+
+    remoteConnection.ondatachannel = (e) => {
+      remoteConnection.dc = e.channel;
+      remoteConnection.dc.onmessage = (e) =>
+        logger("Remote", "New message from client! " + e.data);
+      remoteConnection.dc.onopen = (e) =>
+        logger("Remote", "Connection Opened!!");
+    };
+
+    await remoteConnection.setRemoteDescription(offer);
+    logger("Remote", "Offer Set successfully!");
+
+    const answer = await remoteConnection.createAnswer();
+    await remoteConnection.setLocalDescription(answer);
+    logger("Remote", "Answer created");
+    const remoteSDP = await waitToCompleteIceGathering(remoteConnection);
+    sendAnswerToClient(remoteSDP);
+  }
+
+  function sendAnswerToClient(remoteSDP) {
+    const answer = remoteSDP;
+    localConnection.setRemoteDescription(answer);
+  }
+
+  function waitToCompleteIceGathering(pc) {
+    return new Promise((resolve) => {
+      pc.addEventListener(
+        "icegatheringstatechange",
+        (e) =>
+          e.target.iceGatheringState === "complete" &&
+          resolve(pc.localDescription)
+      );
+    });
+  }
+
+  function logger(connectionType, message) {
+    console.log(`[${connectionType}] ${message}`);
   }
 });
